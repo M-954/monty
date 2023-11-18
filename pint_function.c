e523eca044e645bdd38e84561942c9c1961da39d
Maurine <maurinek04@gmail.com>
2023-11-18 18:02:51 +0000

    prints the element at the top of the stack

--- pint_function.c
+++ pint_function.c
@@ -6,12 +6,13 @@
  */
 void pint_function(stack_t **stack, unsigned int line_number)
 {
-	int value = (*stack)->n;
+	int value;
 
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	value = (*stack)->n;
 	fprintf(stdout, "%d\n", value);
 }
